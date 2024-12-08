18a98b3488d9e49d07126b6b30bc0a38dbe33e30
ABDO-OS <abduos369@gmail.com>
2024-12-09 01:20:13 +0200

    السيسي

--- bloc-main/localization/lib/main.dart
+++ bloc-main/localization/lib/main.dart
@@ -101,7 +101,8 @@ class MyApp extends StatelessWidget {
           );
         })
 
-        /* BlocBuilder<AppthemeBloc, AppthemeState>(
+        /* 
+        BlocBuilder<AppthemeBloc, AppthemeState>(
         builder: (context, themestate) {
           var theme = themestate is ChangeTheme ? themestate.appTheme : "l";
           return BlocBuilder<ApplanguageBloc, ApplanguageState>(
