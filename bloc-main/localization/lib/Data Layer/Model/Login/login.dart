16647cce3479141cd6cfa337b57762ec38df3515
ABDO-OS <abduos369@gmail.com>
2024-12-31 22:41:42 +0200

    change in ui

--- bloc-main/localization/lib/Data Layer/Model/Login/login.dart
+++ bloc-main/localization/lib/Data Layer/Model/Login/login.dart
@@ -14,10 +14,11 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   TextEditingController userID = TextEditingController();
   TextEditingController userPassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomInset: false,
       body: BlocListener<CharactersBloc, CharactersState>(
         listener: (context, state) {
           if (state is CharactersLoadedState) {
@@ -25,130 +26,152 @@ class _LoginScreenState extends State<LoginScreen> {
                 MaterialPageRoute(builder: (context) => LayoutScreen()));
           }
         },
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage(
-                    "assets/images/d4af32dda6440ae6eae3ade9a2dca53e.jpg"),
-                fit: BoxFit.fill),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Title(
-                      child: Text(
-                        "WELCOME BACK",
-                        style: TextStyle(fontSize: 40),
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                      "assets/images/d4af32dda6440ae6eae3ade9a2dca53e.jpg"),
+                  fit: BoxFit.fill),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(height: 60),
+                      Title(
+                        child: Text(
+                          "WELCOME BACK",
+                          style: TextStyle(fontSize: 40, color: Colors.white),
+                        ),
+                        color: Colors.white,
                       ),
-                      color: Colors.white,
-                    ),
-                    Title(
-                      child: Text(
-                        "Login to the GOT WORLD",
-                        style: TextStyle(fontSize: 15),
+                      Title(
+                        child: Text(
+                          "Login to the GOT WORLD",
+                          style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                        color: Colors.white,
                       ),
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
-                TextField(
-                  controller: userID,
-                  obscureText: false,
-                  keyboardType: TextInputType.number,
-                  onChanged: (value) {
-                    userID.text = value;
-                  },
-                  onSubmitted: (value) {
-                    userID.text = value;
-                  },
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
+                    ],
                   ),
-                  decoration: InputDecoration(
-                    hintText: "Enter the user id",
-                    hintStyle:
-                        TextStyle(color: const Color.fromARGB(255, 42, 42, 42)),
-                    // enabledBorder: OutlineInputBorder(
-                    //   borderRadius: BorderRadius.circular(15),
-                    //   borderSide: BorderSide(
-                    //     color: const Color.fromARGB(255, 203, 203, 203),
-                    //     width: 3,
-                    //   ),
-                    // ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(
-                        color: const Color.fromARGB(255, 160, 160, 160),
-                        width: 3,
+                  Column(
+                    children: [
+                      TextField(
+                        controller: userID,
+                        obscureText: false,
+                        keyboardType: TextInputType.number,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: const Color.fromARGB(
+                              255, 0, 0, 0), // Inner text color
+                        ),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white.withOpacity(
+                              0.8), // Background color of the TextField
+                          hintText: "Enter the user id",
+                          hintStyle: TextStyle(
+                              color: const Color.fromARGB(255, 0, 0, 0)
+                                  .withOpacity(0.7)), // Hint text color
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(
+                              color: Colors.white, // Border color
+                              width: 2,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(
+                              color:
+                                  Colors.white, // Border color when not focused
+                              width: 2,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(
+                              color: const Color.fromARGB(255, 87, 169,
+                                  135), // Border color when focused
+                              width: 2,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                    fillColor: Color.fromARGB(255, 201, 201, 201),
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextField(
-                  controller: userPassword,
-                  obscureText: true,
-                  onChanged: (value) {
-                    userPassword.text = value;
-                  },
-                  onSubmitted: (value) {
-                    userPassword.text = value;
-                  },
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                  decoration: InputDecoration(
-                    hintText: "Enter the user password",
-                    hintStyle:
-                        TextStyle(color: const Color.fromARGB(255, 42, 42, 42)),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(
-                        color: const Color.fromARGB(255, 203, 203, 203),
-                        width: 3,
+                      SizedBox(
+                        height: 15,
                       ),
-                    ),
-                    fillColor: Color.fromARGB(255, 201, 201, 201),
+                      TextField(
+                        controller: userPassword,
+                        obscureText: true,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: const Color.fromARGB(
+                              255, 0, 0, 0), // Inner text color
+                        ),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white.withOpacity(
+                              0.8), // Background color of the TextField
+                          hintText: "Enter the user password",
+                          hintStyle: TextStyle(
+                              color: const Color.fromARGB(255, 0, 0, 0)
+                                  .withOpacity(0.7)), // Hint text color
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(
+                              color: Colors.white, // Border color
+                              width: 2,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(
+                              color:
+                                  Colors.white, // Border color when not focused
+                              width: 2,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(
+                              color: const Color.fromARGB(255, 87, 169,
+                                  135), // Border color when focused
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 60,
+                      ),
+                      Container(
+                        height: 60,
+                        width: 250,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            BlocProvider.of<CharactersBloc>(context)
+                                .add(LoginEvent());
+                          },
+                          child: Text(
+                            "login",
+                            style: TextStyle(
+                                fontSize: 25,
+                                color: const Color.fromARGB(255, 42, 42, 42)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 50),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 60,
-                  width: 250,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      BlocProvider.of<CharactersBloc>(context)
-                          .add(LoginEvent());
-                    },
-                    child: Text(
-                      "login",
-                      style: TextStyle(
-                          fontSize: 25,
-                          color: const Color.fromARGB(255, 42, 42, 42)),
-                    ),
-                  ),
-                )
-              ],
+                ],
+              ),
             ),
           ),
         ),
